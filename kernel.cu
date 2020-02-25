82ad264d7dec96ea1b1af625fd382eae67a91b94
FatimaHojeij <fatimahojeij97@gmail.com>
2020-02-25 17:31:58 +0200

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -9,42 +9,32 @@ __constant__ float mask_c[MASK_DIM][MASK_DIM];
 
 __global__ void convolution_tiled_kernel(float* input, float* output, unsigned int width, unsigned int height) {
 
+	__shared__ float input_s[IN_TILE_DIM][IN_TILE_DIM];
     // TODO
-    __shared__ float input_s[IN_TILE_DIM][IN_TILE_DIM];
-
-
-
-    unsigned int outRow = blockIdx.y*OUT_TILE_DIM + threadIdx.y;
-    unsigned int outCol = blockIdx.x*OUT_TILE_DIM + threadIdx.x;
-    
-    //to load
-    unsigned int inRow = outRow - MASK_RADIUS;
+	unsigned int outRow = blockIdx.y*OUT_TILE_DIM + threadIdx.y;
+	unsigned int outCol = blockIdx.x*OUT_TILE_DIM + threadIdx.x;
+	unsigned int inRow = outRow - MASK_RADIUS;
     unsigned int inCol = outCol - MASK_RADIUS;
-
-        if(inRow>=0 && inRow < height && inCol >=0 && inCol <width)
-            input_s[threadIdx.y][threadIdx.x]= input[inRow*width + inCol];
-        else 
-            input_s[threadIdx.y][threadIdx.x]=0.0f;
-        __syncthreads();
-        
-        float sum = 0.0f;
-        if(threadIdx.y < OUT_TILE_DIM && threadIdx.x < OUT_TILE_DIM){
-            for(i=0; i<MASK_DIM;i++){
-                for(j=0;j<MASK_DIM;j++){
-                    sum+=mask_c[i][j]*input_s[i+threadIdx.y][j+threadIdx.x];
-                }
-            }
-            if(outRow < height && outCol< width){
-                outCol[outRow*width+outCol]=sum;
+	if (inRow < height && inRow >= 0 && inCol < width && inCol >= 0) {
+		input_s[threadIdx.y][threadIdx.x] = input[inRow*width + inCol];
+	}else{
+		input_s[threadIdx.y][threadIdx.x] = 0;
+	}
+	__syncthreads();
+	
+	//subset of threads
+	if(threadIdx.y < OUT_TILE_DIM && threadIdx.x < OUT_TILE_DIM){
+		float sum = 0.0f;
+        for(int maskRow = 0; maskRow < MASK_DIM; ++maskRow) {
+            for(int maskCol = 0; maskCol < MASK_DIM; ++maskCol) {
+                    sum += mask_c[maskRow][maskCol]*input_s[maskRow + threadIdx.y][maskCol + threadIdx.x];  
             }
         }
-        
-    
-
-
-
-
-
+		if(outRow < height && outCol< width){
+			output[outRow*width + outCol] = sum;
+		}
+		
+	}
 }
 
 void copyMaskToGPU(float mask[][MASK_DIM]) {
@@ -52,7 +42,7 @@ void copyMaskToGPU(float mask[][MASK_DIM]) {
     // Copy mask to constant memory
 
     // TODO
-    cudaMemcpyToSymbol(mask_c, mask, MASK_DIM*MASK_DIM*sizeof(float));
+	cudaMemcpyToSymbol(mask_c, mask, MASK_DIM*MASK_DIM*sizeof(float));
 
 }
 
@@ -61,10 +51,9 @@ void convolution_tiled_gpu(float* input_d, float* output_d, unsigned int width, 
     // Call kernel
 
     // TODO
-    dim3 numThreadsPerBlock(IN_TILE_DIM, IN_TILE_DIM);
-    dim3 numBlocks((width + OUT_TILE_DIM - 1)/OUT_TILE_DIM, (height + OUT_TILE_DIM - 1)/OUT_TILE_DIM);
-    convolution_tiled_kernel <<< numBlocks, numThreadsPerBlock >>> (input_d, output_d, width, height);
-
+	dim3 numThreadsPerBlock(IN_TILE_DIM, IN_TILE_DIM);
+	dim3 numBlocks((width + OUT_TILE_DIM - 1)/OUT_TILE_DIM, (height + OUT_TILE_DIM - 1)/OUT_TILE_DIM);
+	convolution_tiled_kernel <<< numBlocks, numThreadsPerBlock >>> (input_d, output_d, width, height);
 
 
 }
