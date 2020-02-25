1ca3c15117f3028ecb80da06b16d7b953ba39f17
FatimaHojeij <fatimahojeij97@gmail.com>
2020-02-25 11:22:17 +0200

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -9,19 +9,30 @@ __constant__ float mask_c[MASK_DIM][MASK_DIM];
 
 __global__ void convolution_tiled_kernel(float* input, float* output, unsigned int width, unsigned int height) {
 
+	__shared__ float input_s[IN_TILE_DIM][IN_TILE_DIM];
     // TODO
+	int outRow = blockIdx.y*OUT_TILE_DIM + threadIdx.y;
+	int outCol = blockIdx.x*OUT_TILE_DIM + threadIdx.x;
 	int inRow = blockIdx.y*blockDim.y + threadIdx.y;
 	int inCol = blockIdx.x*blockDim.x + threadIdx.x;
 	if (inRow < height && inRow >= 0 && inCol < width && inCol >= 0) {
-		float sum = 0.0f; 
-		for(int maskRow = 0; maskRow < MASK_DIM; ++maskRow) {
-			for(int maskCol = 0; maskCol < MASK_DIM; ++maskCol) {
-				sum += mask_c[maskRow][maskCol]*input[inRow*width + inCol]; 
-			}
-		} 
-		if(inRow<OUT_TILE_DIM && inCol<OUT_TILE_DIM ){
-			output[inRow*width + outCol] = sum; 
-		}
+		input_s[threadIdx.y][threadIdx.x] = input[inRow*width + inCol];
+	}
+	__syncthreads();
+	
+	if(inRow<OUT_TILE_DIM && inCol<OUT_TILE_DIM ){
+		float sum = 0.0f;
+        for(int maskRow = 0; maskRow < MASK_DIM; ++maskRow) {
+            for(int maskCol = 0; maskCol < MASK_DIM; ++maskCol) {
+                int inRow = outRow - MASK_RADIUS + maskRow;
+                int inCol = outCol - MASK_RADIUS + maskCol;
+                if(inRow >= 0 && inRow < height && inCol >= 0 && inCol < width) {
+                    sum += mask_c_[maskRow][maskCol]*input_s[inRow*width + inCol];
+                }
+            }
+			__syncthreads();
+        }
+        output[outRow*width + outCol] = sum;
 	}
 }
 
